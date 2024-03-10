4079c26f9b3e55994d33d91916bf4c092ad9c3cf
zenpk <99558412+zenpk@users.noreply.github.com>
2024-03-10 16:26:16 +0000

    update

--- lush/lua/lush_theme/jeteyes.lua
+++ lush/lua/lush_theme/jeteyes.lua
@@ -114,7 +114,7 @@ local theme = lush(function(injected_functions)
 		-- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
 		Search         { bg = c.searchOthers }, -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
 		SpecialKey     { fg = c.green.darken(30) }, -- Unprintable characters: text displayed differently from what it really is. But not 'listchars' whitespace. |hl-Whitespace|
-		SpellBad       { undercurl = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
+		SpellBad       { sp = c.green, underline = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
 		SpellCap       { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
 		SpellLocal     { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
 		SpellRare      { }, -- Word that is recognized by the spellchecker as one that is hardly ever used. |spell| Combined with the highlighting used otherwise.
